929db10efd96947cf06e3cffe31935c27a7fb5bb
Emmanuel Castañeda Cano <ecastanedc@eafit.edu.co>
2024-08-19 16:14:15 -0500

    inicial

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render # here by default
+from django.http import HttpResponse # new# Create your views here.
 
-# Create your views here.
+
+def homePageView(request): # new
+    return HttpResponse('Hello World!') # new
