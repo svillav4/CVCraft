5a7c03d577937bdbf3e9bb93f6063fb9c3540e2e
Juanda151005 <juandavidvr151005@gmail.com>
2024-11-05 07:58:43 -0500

    Improving pdf function

--- CVCraft/recomendations/urls.py
+++ CVCraft/recomendations/urls.py
@@ -4,6 +4,6 @@ from .views import *
 urlpatterns = [
     path('subprofile/', subprofile, name='subprofile'),
     path('selected_subprofile/<int:subprofile_id>/<int:index>/',selected_subprofile, name='selected_subprofile'),
-    path('download_subprofile/<int:subprofile_id>/<int:index>/',download_pdf, name='download'),
+    path('download_subprofile/<int:id>/<int:indice>/',download_pdf, name='download'),
     path('no-subprofile/', subprofile, name='no_subprofile'),
 ]
