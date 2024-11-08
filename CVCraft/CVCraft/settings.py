17230d2b626c59dce33b90515b420e230e49e730
TheSamVil <svillav4@eafit.edu.co>
2024-11-08 10:20:35 -0500

    deployment

--- CVCraft/CVCraft/settings.py
+++ CVCraft/CVCraft/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^(%ip3nl3isk#4y0si1wt($20tio3ze^#y%3q73_77=dkgxirn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.29.196.108']
 
 
 # Application definition
